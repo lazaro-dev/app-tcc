1211c5fccb4dabc64aa8a25b7c21246d6eb0f438
unknown <lazaro.sobral@facape.br>
2021-03-26 14:46:55 -0300

    add: notify

---
+++ src/app/notify/notify.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notify',
+  templateUrl: './notify.component.html',
+  styleUrls: ['./notify.component.scss'],
+})
+export class NotifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
